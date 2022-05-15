a2bfbaef3a26dd4bda41c78a154a102077ac4fcd
Wenjun Xiao <wenjunxiao@126.com>
2022-05-15 20:35:20 +0800

    add clear cookies

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
-    <div>
-      <el-button @click="decodeSQL">SQL解码</el-button>
-    </div>
+    <el-row justify="start">
+      <el-col span="24">
+        <el-button @click="decodeSQL" style="width:100%;">SQL解码</el-button>
+      </el-col>
+    </el-row>
+    <br/>
+    <el-row justify="start">
+      <el-col span="12">
+        <el-button @click="clearCookie" style="width:100%;">清理Cookie</el-button>
+      </el-col>
+      <el-col span="12">
+        <el-input v-model="cookieName" />
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -10,6 +21,11 @@
 import { ACTION_SQL_DECODER } from '../constants'
 
 export default {
+  data() {
+    return {
+      cookieName: 'SESSION'
+    }
+  },
   methods: {
     decodeSQL () {
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -20,6 +36,26 @@ export default {
         }, function (response) {
           console.log('contextMenus.response>', tab.id, response);
         });
+      });
+    },
+    clearCookie () {
+      const cookieName = this.cookieName || 'SESSION';
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const url = tabs[0].url;
+        console.log('tab =>', url);
+        if (/^(https?):\/\/([^/]+)/.test(url)) {
+          var domain = RegExp.$2;
+          var protocol = RegExp.$1;
+          var parts = domain.split('.');
+          for (let i = 0, m = Math.max(parts.length - 2, 0); i < m; i++) {
+            chrome.cookies.remove({
+              name: cookieName,
+              url: protocol + '://' + parts.slice(i).join('.')
+            }, function (cookie) {
+              console.log('remove cookie =>', cookie);
+            })
+          }
+        }
       });
     }
   }
@@ -34,5 +70,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 10px;
+  width: 300px;
 }
 </style>
