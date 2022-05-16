0d8ae3bc3bcffd2259fef63998b46790759ff423
Wenjun Xiao <wenjunxiao@126.com>
2022-05-16 23:43:02 +0800

    fix clear cookies

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -39,21 +39,24 @@ export default {
       });
     },
     clearCookie () {
-      const cookieName = this.cookieName || 'SESSION';
+      const name = this.cookieName || 'SESSION';
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const url = tabs[0].url;
-        console.log('tab =>', url);
-        if (/^(https?):\/\/([^/]+)/.test(url)) {
+        const tabUrl = tabs[0].url;
+        if (/^(https?):\/\/([^/]+)(.*)$/.test(tabUrl)) {
           var domain = RegExp.$2;
-          var protocol = RegExp.$1;
+          var path = RegExp.$3;
+          var protocols = new Set([RegExp.$1, 'https']);
           var parts = domain.split('.');
           for (let i = 0, m = Math.max(parts.length - 2, 0); i < m; i++) {
-            chrome.cookies.remove({
-              name: cookieName,
-              url: protocol + '://' + parts.slice(i).join('.')
-            }, function (cookie) {
-              console.log('remove cookie =>', cookie);
-            })
+            for (let protocol of protocols) {
+              let url = protocol + '://' + parts.slice(i).join('.') + path;
+              chrome.cookies.remove({
+                name,
+                url
+              }, function (cookie) {
+                console.log('remove cookie =>', url, cookie);
+              })
+            }
           }
         }
       });
